5961b95293794fbe9cd7cf64d5cef3f4edb5ffaa
Лямин <genrih.md@gmail.com>
2020-11-03 16:00:24 +0300

    Add welcome message

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-import execa from 'execa'
-import Listr from 'listr'
+const execa = require('execa')
+const Listr = require('listr')
+const chalk = require('chalk')
+
+console.log(chalk.blue('FE is a frontend cli toolbox!'))
 
 const tasks = new Listr([
-    
+    {
+		title: 'Success',
+		task: () => 'Foo'
+	},
 ])
 
 tasks.run().catch( e => { } )
