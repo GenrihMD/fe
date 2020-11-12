22ef60ce7155fdc28583b60b1cf4c03ce04ff128
Лямин <genrih.md@gmail.com>
2020-11-12 12:02:44 +0300

    Add simple parsing

--- tasker/parser.js
+++ tasker/parser.js
@@ -29,4 +29,15 @@ const
         const action = words[0]
         const args = words.slice(1, words.lenght)
 
+        switch (action) {
+            case ( 'add' ): return getAdd(args[0])
+            case ( 'remove' ): return getRemove(args[0])
+            case ( 'move' ): return getMove(args[0], args[1])
+            case ( 'rename' ): return getRename(args[0], args[1])
+            case ( 'copy' ): return getCopy(args[0], args[1])
+        }
     }
+
+public: {
+    parser.parse = parse
+}
